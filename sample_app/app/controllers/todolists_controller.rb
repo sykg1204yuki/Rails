1f7f73c668dbf733f7a3ee5a5966a5ad2f4d5dcd
EC2 Default User <ec2-user@ip-172-31-25-149.ec2.internal>
2021-05-27 07:25:56 +0000

    [start]2-7 画像投稿機能作成

--- sample_app/app/controllers/todolists_controller.rb
+++ sample_app/app/controllers/todolists_controller.rb
@@ -2,29 +2,41 @@ class TodolistsController < ApplicationController
   def new
     @list = List.new
   end
-  
+
   def create
     list = List.new(list_params) #データの新規登録
     list.save #データの保存
     redirect_to todolist_path(list.id) #詳細画面へリダイレクト
-  end 
-  
+  end
+
+
   def index
     @lists = List.all
-  end 
-  
+  end
+
   def show
     @list = List.find(params[:id])
+  end
+
+
+  def edit
+    @list = List.find(params[:id])
+  end
+  
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to todolist_path(list.id)
   end 
-  
-  
+
+
   private
   #ストロングパラメータ
   def list_params
     params.require(:list).permit(:title,:body)
-  end 
-  
-  
+  end
+
+
 end
 
 
