03707c3afdef3e2950689e0eb8b1308546e6a449
EC2 Default User <ec2-user@ip-172-31-25-149.ec2.internal>
2021-05-27 04:45:58 +0000

    app delete

--- sample_app/app/models/list.rb
+++
@@ -1,3 +0,0 @@
-class List < ApplicationRecord
-  attachment :image
-end
--- sample_app/config/initializers/application_controller_renderer.rb
+++
@@ -1,9 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# ActiveSupport::Reloader.to_prepare do
-#   ApplicationController.renderer.defaults.merge!(
-#     http_host: 'example.org',
-#     https: false
-#   )
-# end
-Refile.secret_key = 'f0489a0f903ffa51d03c5d686ffc6099bb72dc9632a5fa642ca1533fe4079f7b3335dd3c24ed6f0ca1ab17043d13fc6657002069bd628f047842960d343d257c'
